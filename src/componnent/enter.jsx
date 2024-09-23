df73ba96c49432fd9d9122524ccd03a11e4c60d1
Thony0666 <160373228+Anthonyo02@users.noreply.github.com>
2024-09-23 11:55:16 +0300

    add logique to create the graph

--- src/componnent/enter.jsx
+++ src/componnent/enter.jsx
@@ -1,77 +1,101 @@
-import React from "react";
+import React, { useState } from 'react';
+import { Form } from 'react-bootstrap';
+import '../App.css';
 
-function Enter(){
-    return<>
-    <div className="w-[40%] flex h-[20vh] flex-col">
-    <div>
-        <label for="currency" class="sr-only">Patrimoine</label>
-        <select id="currency" name="Patrimoine" >
-            <option>Cresus</option>
-            <option>Beba</option>
-            <option>Koto</option>
-        </select>
-    </div>
-    <div className="flex flex-row justify-between">
-        <div>
-            <input
-            class="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-secondary-500 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-primary dark:checked:bg-primary"
-            type="checkbox"
-            value=""
-            id="checkboxDefault" />
-            <label
-                class="inline-block ps-[0.15rem] hover:cursor-pointer"
-                for="checkboxDefault">
-                Agregat
-            </label>
-        </div>
-    <div>
-        <input
-        class="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-secondary-500 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-primary dark:checked:bg-primary"
-        type="checkbox"
-        value=""
-        id="checkboxDefault" />
-        <label
-            class="inline-block ps-[0.15rem] hover:cursor-pointer"
-            for="checkboxDefault">
-            Tresorerie
-        </label>
-    </div>
-    <div>
-        <input
-        class="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-secondary-500 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-primary dark:checked:bg-primary"
-        type="checkbox"
-        value=""
-        id="checkboxDefault" />
-        <label
-            class="inline-block ps-[0.15rem] hover:cursor-pointer"
-            for="checkboxDefault">
-            Immobilisation
-        </label>
-    </div>
-    <div>
-        <input
-        class="relative float-left -ms-[1.5rem] me-[6px] mt-[0.15rem] h-[1.125rem] w-[1.125rem] appearance-none rounded-[0.25rem] border-[0.125rem] border-solid border-secondary-500 outline-none before:pointer-events-none before:absolute before:h-[0.875rem] before:w-[0.875rem] before:scale-0 before:rounded-full before:bg-transparent before:opacity-0 before:shadow-checkbox before:shadow-transparent before:content-[''] checked:border-primary checked:bg-primary checked:before:opacity-[0.16] checked:after:absolute checked:after:-mt-px checked:after:ms-[0.25rem] checked:after:block checked:after:h-[0.8125rem] checked:after:w-[0.375rem] checked:after:rotate-45 checked:after:border-[0.125rem] checked:after:border-l-0 checked:after:border-t-0 checked:after:border-solid checked:after:border-white checked:after:bg-transparent checked:after:content-[''] hover:cursor-pointer hover:before:opacity-[0.04] hover:before:shadow-black/60 focus:shadow-none focus:transition-[border-color_0.2s] focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-black/60 focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-[0.875rem] focus:after:w-[0.875rem] focus:after:rounded-[0.125rem] focus:after:content-[''] checked:focus:before:scale-100 checked:focus:before:shadow-checkbox checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] checked:focus:after:-mt-px checked:focus:after:ms-[0.25rem] checked:focus:after:h-[0.8125rem] checked:focus:after:w-[0.375rem] checked:focus:after:rotate-45 checked:focus:after:rounded-none checked:focus:after:border-[0.125rem] checked:focus:after:border-l-0 checked:focus:after:border-t-0 checked:focus:after:border-solid checked:focus:after:border-white checked:focus:after:bg-transparent rtl:float-right dark:border-neutral-400 dark:checked:border-primary dark:checked:bg-primary"
-        type="checkbox"
-        value=""
-        id="checkboxDefault" />
-        <label
-            class="inline-block ps-[0.15rem] hover:cursor-pointer"
-            for="checkboxDefault">
-            Obligations
-        </label>
-    </div>
-    </div>
-    <div>
-        <div>
-        <label htmlFor="De">De</label>
-        <input type="date" name="" id="De" />
-        </div>
-        <div>
-        <label htmlFor="A">A</label>
-        <input type="date" name="" id="A" />
-        </div>
-    </div>
-    </div>
-    </>
+function Enter({ setGraphData }) {
+    const [checkedItems, setCheckedItems] = useState({
+        agregrat: false,
+        tresorerie: false,
+        immobilisation: false,
+        obligations: false,
+    });
+
+    const handleCheckboxChange = (event) => {
+        const { name, checked } = event.target;
+        setCheckedItems({ ...checkedItems, [name]: checked });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Empêche le rechargement de la page
+
+        // Créer les données pour le graphique
+        const graphData = [
+            { name: 'Agregrat', value: checkedItems.agregrat ? 1 : 0 },
+            { name: 'Tresorerie', value: checkedItems.tresorerie ? 1 : 0 },
+            { name: 'Immobilisation', value: checkedItems.immobilisation ? 1 : 0 },
+            { name: 'Obligations', value: checkedItems.obligations ? 1 : 0 },
+        ];
+
+        setGraphData(graphData); // Met à jour l'état dans le parent
+    };
+
+    return (
+        <>
+            <div className="custom-class">
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label htmlFor="currency" className="sr-only">Patrimoine</label>
+                        <select id="currency" name="Patrimoine">
+                            <option>Cresus</option>
+                            <option>Beba</option>
+                            <option>Koto</option>
+                        </select>
+                    </div>
+                    <div>
+                        <Form className="layout-flex-row">
+                            {['checkbox'].map((type) => (
+                                <div key={`inline-${type}`} className="mb-3 layout-flex-row">
+                                    <Form.Check
+                                        inline
+                                        label="Agregrat"
+                                        name="agregrat"
+                                        type={type}
+                                        checked={checkedItems.agregrat}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <Form.Check
+                                        inline
+                                        label="Tresorerie"
+                                        name="tresorerie"
+                                        type={type}
+                                        checked={checkedItems.tresorerie}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <Form.Check
+                                        inline
+                                        label="Immobilisation"
+                                        name="immobilisation"
+                                        type={type}
+                                        checked={checkedItems.immobilisation}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                    <Form.Check
+                                        inline
+                                        label="Obligations"
+                                        name="obligations"
+                                        type={type}
+                                        checked={checkedItems.obligations}
+                                        onChange={handleCheckboxChange}
+                                    />
+                                </div>
+                            ))}
+                        </Form>
+                    </div>
+                    <div className="layout-flex-row">
+                        <div className="custom-width">
+                            <label htmlFor="De">De</label>
+                            <input type="date" id="De" />
+                        </div>
+                        <div className="custom-width">
+                            <label htmlFor="A">A</label>
+                            <input type="date" id="A" />
+                        </div>
+                    </div>
+                    <button type="submit">Voir le résultat</button>
+                </form>
+            </div>
+        </>
+    );
 }
+
 export default Enter;
